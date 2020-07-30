7c3fe1c182490a23a9df195ee8eaa0637cea919c
carlosvitorlps <carlosvitorlps@gmail.com>
2020-07-29 21:27:00 -0300

    update the example of the data structure comment on it to identify how to use it

---
+++ data_list example.py
@@ -0,0 +1,6 @@
+insta_users = ["@name1", "@name2", "@name3"]
+
+# i. e. in this link: https://www.instagram.com/p/CDPaQrtHGa3/
+# would be p/CDPaQrtHGa3
+post_id = "p/CDPaQrtHGa3"
+# post_id = "change to which one you want"
